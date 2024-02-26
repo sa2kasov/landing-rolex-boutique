2a637dc7c7917be0f21284b0d8f31a9a8da37f0c
Dias <dias.sadvakasov@gmail.com>
2024-02-26 21:34:54 +0600

    "section" folder created
    The structure of all necessary components has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
+import {
+  Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer
+} from './sections'
+
 const App = () => (
   <main className="relative">
-    Nav
+    {/*<Nav />*/}
     <section className="xl:padding-l wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="padding-x padding-t pb-8 bg-black">
-      Footer
+      <Footer />
     </section>
   </main>
 )
